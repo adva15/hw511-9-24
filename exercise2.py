01e002d634885afac25d82e99759640f2c89bfa3
adva15 <aadva5319@gmail.com>
2024-09-14 22:04:04 +0300

    Add files via upload

---
+++ exercise2.py
@@ -0,0 +1,39 @@
+
+#start
+#1
+
+for i in range(0, 10 + 1, 1):
+    print(i, end=" ")
+print()
+
+#end
+
+#start
+#2
+
+for i in range(40, 88 + 1, 1):
+    print(i, end=" ")
+print()
+
+#end
+
+
+#start
+#3
+
+for i in range(77, 106, 7):
+    print(i, end=" ")
+print()
+
+#end
+
+#start
+#4
+
+for i in range(99, 66-1, -3):
+    print(i, end=" ")
+print()
+
+#end
+
+
